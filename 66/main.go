fdee364c9f576ef399b060bb2bb34a83cab4309f
Lorran Sutter <sutterlorran@gmail.com>
2025-09-19 20:07:09 -0400

    66

---
+++ 66/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		newDigit := digits[i] + 1
+		if newDigit <= 9 {
+			digits[i] = newDigit
+			return digits
+		}
+
+		digits[i] = 0
+
+		if i == 0 {
+			return append([]int{1}, digits...)
+		}
+	}
+
+	return digits
+}
+
+func main() {
+	fmt.Println(plusOne([]int{1, 2, 3}))
+	fmt.Println(plusOne([]int{4, 3, 2, 1}))
+	fmt.Println(plusOne([]int{9}))
+	fmt.Println(plusOne([]int{8, 9, 9, 9}))
+}
