cd5faeb75090af92300645c108ca4dcd858e0cb7
Lorran Sutter <sutterlorran@gmail.com>
2025-09-25 20:59:30 -0400

    75 solution 2

--- 75/main.go
+++ 75/main.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-func sortColors(nums []int) {
+// 0 ms / 4.26 MB
+func sortColors1(nums []int) {
 	quicksort(&nums, 0, len(nums)-1)
 
 	fmt.Println(nums)
@@ -34,7 +35,40 @@ func partition(nums *[]int, lo int, hi int) int {
 
 }
 
+// 0 ms / 4.08 MB
+func sortColors2(nums []int) {
+	freqs := make([]int, 3)
+
+	// Set frequencies for each color
+	for _, num := range nums {
+		freqs[num]++
+	}
+
+	currentSetColor := 0
+	for i := range nums {
+	StartLoop:
+		if freqs[currentSetColor] == 0 {
+			// If no color left, go to the next one
+			currentSetColor++
+			goto StartLoop
+		}
+		freqs[currentSetColor]--
+		nums[i] = currentSetColor
+	}
+
+	fmt.Println(nums)
+}
+
 func main() {
-	sortColors([]int{2, 0, 2, 1, 1, 0})
-	sortColors([]int{2, 0, 1})
+	fmt.Println("Solution 1")
+	sortColors1([]int{2, 0, 2, 1, 1, 0})
+	sortColors1([]int{2, 0, 1})
+	sortColors1([]int{2})
+
+	fmt.Println()
+
+	fmt.Println("Solution 2")
+	sortColors2([]int{2, 0, 2, 1, 1, 0})
+	sortColors2([]int{2, 0, 1})
+	sortColors2([]int{2})
 }
