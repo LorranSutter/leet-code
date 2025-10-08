452ec4575b94b2231293588a55ea525eb909d99a
Lorran Sutter <sutterlorran@gmail.com>
2025-10-08 16:37:08 -0400

    3701 other solutions

--- 3701/main.py
+++ 3701/main.py
@@ -2,8 +2,22 @@ from typing import List
 
 
 class Solution:
-    def alternatingSum(self, nums: List[int]) -> int:
+    # 3 ms / 18.12 MB
+    def alternatingSum1(self, nums: List[int]) -> int:
         res = 0
         for i in range(len(nums)):
             res += nums[i] if i % 2 == 0 else -nums[i]
+        return res
+
+    # 3 ms / 17.89 ms
+    def alternatingSum2(self, nums: List[int]) -> int:
+        res = sum((nums[i] for i in range(0, len(nums), 2)))
+        res += sum((-nums[i] for i in range(1, len(nums), 2)))
+        return res
+
+    # 4 ms / 17.82 ms
+    def alternatingSum3(self, nums: List[int]) -> int:
+        res = 0
+        for i, num in enumerate(nums):
+            res += (-1) ** i * num
         return res
