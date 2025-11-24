59093e9501fbe337db1fab7777c4c796f900cad0
LorranSutter <sutterlorran@gmail.com>
2025-11-24 17:41:35 -0500

    1161

---
+++ 1161/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"leetcode/utils"
+	"math"
+)
+
+// The idea here is to perform a level order traversal (BFS) of the tree,
+// calculating the sum of node values at each level.
+// We keep track of the maximum sum encountered and the corresponding level.
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxLevelSum(root *utils.TreeNode) int {
+	queue := []*utils.TreeNode{root}
+	minLevel, maxSum := 1, int(math.Inf(-1))
+
+	level, levelSum := minLevel, 0
+	var node *utils.TreeNode
+	for len(queue) > 0 {
+		levelSize := len(queue)
+
+		for range levelSize {
+			node = queue[0]
+			queue = queue[1:]
+			levelSum += node.Val
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+
+		if levelSum > maxSum {
+			maxSum = levelSum
+			minLevel = level
+		}
+
+		levelSum = 0
+		level++
+	}
+
+	return minLevel
+}
+
+func main() {
+	root := utils.MakeBinaryTreeFromLevelOrder([]int{1, 7, 0, 7, -8, int(math.Inf(1)), int(math.Inf(1))}, int(math.Inf(1)))
+	fmt.Println(maxLevelSum(root) == 2)
+
+	root = utils.MakeBinaryTreeFromLevelOrder([]int{989, int(math.Inf(1)), 10250, 98693, -89388, int(math.Inf(1)), int(math.Inf(1)), int(math.Inf(1)), -32127}, int(math.Inf(1)))
+	fmt.Println(maxLevelSum(root) == 2)
+
+	root = utils.MakeBinaryTreeFromLevelOrder([]int{-100, -200, -300, -20, -5, -10, int(math.Inf(1))}, int(math.Inf(1)))
+	fmt.Println(maxLevelSum(root) == 3)
+}
