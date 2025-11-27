1b66c25ed07f835d759908b7ca6273c02850e01e
LorranSutter <sutterlorran@gmail.com>
2025-11-27 16:36:33 -0500

    20

---
+++ 0020/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// The idea here is to use a stack to keep track of the opening parentheses.
+// If we encounter a closing parenthesis, we check if the last opening parenthesis is the corresponding type.
+// If they don't match, we return false.
+// At the end, if the stack is empty, it means all parentheses were matched correctly.
+func isValid(s string) bool {
+	stack := []rune{}
+	pairs := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+
+	for _, char := range s {
+		switch char {
+		case '(', '{', '[':
+			stack = append(stack, char)
+		case ')', '}', ']':
+			if len(stack) == 0 || pairs[char] != stack[len(stack)-1] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
+
+func main() {
+	fmt.Println(isValid("[") == false)
+	fmt.Println(isValid("()") == true)
+	fmt.Println(isValid("()[]{}") == true)
+	fmt.Println(isValid("(]") == false)
+	fmt.Println(isValid("([])") == true)
+	fmt.Println(isValid("([)])") == false)
+	fmt.Println(isValid("([{}])") == true)
+	fmt.Println(isValid("[({})]") == true)
+}
