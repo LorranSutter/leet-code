d81a95ec7c6789c92f445f405a026403d776f3b0
LorranSutter <sutterlorran@gmail.com>
2026-08-10 14:14:14 -0400

    322

---
+++ 0322/main.ts
@@ -0,0 +1,20 @@
+function coinChange(coins: number[], amount: number): number {
+    const dp: number[] = new Array(amount + 1).fill(Infinity);
+    dp[0] = 0;
+
+    for (let a = 1; a <= amount; a++) {
+        for (const coin of coins) {
+            if (coin <= a) {
+                dp[a] = Math.min(dp[a], dp[a - coin] + 1)
+            }
+        }
+    }
+
+    return dp[amount] === Infinity ? -1 : dp[amount];
+}
+
+console.log(coinChange([1, 2, 5, 10], 18))
+console.log(coinChange([186, 419, 83, 408], 6249))
+console.log(coinChange([1, 2, 5], 11));
+console.log(coinChange([2], 3));
+console.log(coinChange([1], 0));
